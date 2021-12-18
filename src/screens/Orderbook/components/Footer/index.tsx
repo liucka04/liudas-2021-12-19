e60be48a8529443dfec7d2ef9d8633ad40539add
Liudas Barcauskas <liudasbarcauskas@gmail.com>
2021-12-18 22:17:05 +0200

    implement click handler in footer

--- src/screens/Orderbook/components/Footer/index.tsx
+++ src/screens/Orderbook/components/Footer/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {Box} from '~/components/Box';
 import {Button} from '~/components/Button';
+import {useToggleProduct} from '../../FeedProvider/hooks/useToggleProduct';
 
 export const OrderbookFooter = () => {
   const {bottom: insetBottom} = useSafeAreaInsets();
+  const {toggleProduct} = useToggleProduct();
+
   const paddingBottom = insetBottom === 0 ? 20 : insetBottom;
 
   return (
     <Box backgroundColor="blue.900" pt={5} marginX={-6} style={{paddingBottom}}>
-      <Button onPress={() => {}}>Toggle Feed</Button>
+      <Button onPress={toggleProduct}>Toggle Feed</Button>
     </Box>
   );
 };

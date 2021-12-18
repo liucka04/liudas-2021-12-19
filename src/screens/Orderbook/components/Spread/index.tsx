0aa418ad22d07ed3feb763c1c9a7e7e872dd599f
Liudas Barcauskas <liudasbarcauskas@gmail.com>
2021-12-18 14:55:56 +0200

    add spread component

---
+++ src/screens/Orderbook/components/Spread/index.tsx
@@ -0,0 +1,19 @@
+import React, {FC} from 'react';
+import {Box} from '~/components/Box';
+import Text from '~/components/Text';
+
+type Props = {
+  spread: {value: string; percentage: number} | null;
+};
+
+export const OrderbookSpread: FC<Props> = ({spread}) => {
+  const {value, percentage} = spread ?? {};
+
+  return (
+    <Box paddingY={2} alignItems="center">
+      <Text color="gray" fontWeight={500}>
+        Spread: {value} ({percentage}%)
+      </Text>
+    </Box>
+  );
+};
---
+++ src/screens/Orderbook/utils/getSpread.ts
@@ -0,0 +1,31 @@
+import _ from 'lodash';
+import {Level} from '~/types';
+
+type GetSpreadParams = {
+  bids: Level[];
+  asks: Level[];
+};
+
+type GetPercentageParams = {
+  spreadValue: number;
+  highestBidPrice: number;
+};
+
+const getPercentage = ({spreadValue, highestBidPrice}: GetPercentageParams) => {
+  return _.round(spreadValue / highestBidPrice, 2);
+};
+
+export const getSpread = ({bids, asks}: GetSpreadParams) => {
+  const highestBidPrice = _.first(bids)?.price;
+  const lowestAskPrice = _.first(asks)?.price;
+
+  if (highestBidPrice && lowestAskPrice) {
+    const spreadValue = Math.abs(highestBidPrice - lowestAskPrice);
+    return {
+      value: _.round(spreadValue, 1).toLocaleString(),
+      percentage: getPercentage({highestBidPrice, spreadValue}),
+    };
+  } else {
+    return null;
+  }
+};

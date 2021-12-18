2b96255cedbeee625bf755ea73adac23b4e78da1
Liudas Barcauskas <liudasbarcauskas@gmail.com>
2021-12-18 12:50:22 +0200

    add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Box} from '../Box';
+import Text from '../Text';
+
+export const Header = () => {
+  return (
+    <Box marginX={-6} paddingX={6}>
+      <Text>Order book</Text>
+    </Box>
+  );
+};

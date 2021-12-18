95bfcb10e038c2dccde22954d47d595f4a09d9e2
Liudas Barcauskas <liudasbarcauskas@gmail.com>
2021-12-18 14:56:45 +0200

    add new components to orderbook

--- src/screens/Orderbook/components/List/index.tsx
+++ src/screens/Orderbook/components/List/index.tsx
@@ -2,24 +2,36 @@ import _ from 'lodash';
 import React, {FC} from 'react';
 import {Box} from '~/components/Box';
 import Text from '~/components/Text';
-import {Level} from '../../types';
+import {Level} from '~/types';
+import {getAboveItemSizes} from './utils/getAboveItemSizes';
 
 type Props = {
   items: Level[];
+  type: 'asks' | 'bids';
 };
 
-export const List: FC<Props> = ({items}) => {
+export const List: FC<Props> = ({items, type}) => {
+  const isBids = type === 'bids';
+
   return (
     <Box>
       {items.map((item, index) => {
-        const aboveItems = _.take(items, index + 1);
-        const total = _.sum(aboveItems.map(aboveItem => aboveItem.size));
+        const total = _.sum(
+          getAboveItemSizes({isBids, itemIndex: index, items}),
+        );
 
         return (
-          <Box paddingY={1} flexDirection="row" justifyContent="space-between">
+          <Box
+            key={item.price.toString()}
+            paddingY={1.5}
+            flexDirection="row"
+            justifyContent="space-between">
             <Box width={4 / 12} justifyContent="flex-end">
-              <Text textAlign="right" fontWeight={700}>
-                {item.price.toLocaleString()}
+              <Text
+                textAlign="right"
+                fontWeight={700}
+                color={isBids ? 'green.500' : 'red.500'}>
+                {item.price.toFixed(2).toLocaleString()}
               </Text>
             </Box>
             <Box width={3 / 12} justifyContent="flex-end">
@@ -27,7 +39,7 @@ export const List: FC<Props> = ({items}) => {
                 {item.size.toLocaleString()}
               </Text>
             </Box>
-            <Box width={3 / 12} justifyContent="flex-end">
+            <Box width={3.5 / 12} justifyContent="flex-end">
               <Text textAlign="right" fontWeight={700}>
                 {total.toLocaleString()}
               </Text>
--- src/screens/Orderbook/index.tsx
+++ src/screens/Orderbook/index.tsx
@@ -2,11 +2,13 @@ import React, {FC} from 'react';
 import {Box} from '~/components/Box';
 import {Header} from '~/components/Header';
 import {Screen} from '~/components/Screen';
-import Text from '~/components/Text';
 import {Error} from './components/Error';
 import {OrderbookFooter} from './components/Footer';
 import {List} from './components/List';
+import {OrderbookListHeader} from './components/ListHeader';
+import {OrderbookSpread} from './components/Spread';
 import {useFeedContext} from './FeedProvider/hooks/useFeedContext';
+import {getSpread} from './utils/getSpread';
 
 export const OrderbookScreen: FC = () => {
   const {error, asks, bids, isLoading} = useFeedContext();
@@ -20,28 +22,15 @@ export const OrderbookScreen: FC = () => {
   }
 
   return (
-    <>
-      <Screen>
-        <Header />
-        <Box flexGrow={0}>
-          <Box paddingY={1} flexDirection="row" justifyContent="space-between">
-            <Box width={4 / 12} justifyContent="flex-end">
-              <Text textAlign="right">Price</Text>
-            </Box>
-            <Box width={3 / 12} justifyContent="flex-end">
-              <Text textAlign="right">Size</Text>
-            </Box>
-            <Box width={3 / 12} justifyContent="flex-end">
-              <Text textAlign="right">Total</Text>
-            </Box>
-          </Box>
-        </Box>
-        <Box flex={1}>
-          <List items={bids} />
-          <List items={asks} />
-        </Box>
-        <OrderbookFooter />
-      </Screen>
-    </>
+    <Screen>
+      <Header />
+      <OrderbookListHeader />
+      <Box flex={1}>
+        <List items={bids} type="asks" />
+        <OrderbookSpread spread={getSpread({asks, bids})} />
+        <List items={asks} type="bids" />
+      </Box>
+      <OrderbookFooter />
+    </Screen>
   );
 };

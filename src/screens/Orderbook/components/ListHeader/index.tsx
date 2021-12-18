390b5322061657d954407bb83aa8034f28d0f3b4
Liudas Barcauskas <liudasbarcauskas@gmail.com>
2021-12-18 22:14:39 +0200

    style list header

--- src/screens/Orderbook/components/ListHeader/index.tsx
+++ src/screens/Orderbook/components/ListHeader/index.tsx
@@ -4,10 +4,22 @@ import Text from '~/components/Text';
 
 export const OrderbookListHeader = () => {
   return (
-    <Box flexGrow={0}>
+    <Box
+      flexGrow={0}
+      borderTopWidth={2}
+      borderBottomWidth={1}
+      marginX={-6}
+      paddingX={6}
+      borderStyle="solid"
+      borderColor="gray">
       <Box paddingY={1} flexDirection="row" justifyContent="space-between">
         <Box width={4 / 12} justifyContent="flex-end">
-          <Text textAlign="right" fontSize="lg" fontWeight={700} color="gray">
+          <Text
+            textAlign="right"
+            fontSize="lg"
+            fontWeight={700}
+            color="gray"
+            textStyle="uppercase">
             Price
           </Text>
         </Box>

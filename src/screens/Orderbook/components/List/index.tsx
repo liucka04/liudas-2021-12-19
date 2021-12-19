ec2a8ab21048237359e2a6d5330539fffb3cf319
Liudas Barcauskas <liudasbarcauskas@gmail.com>
2021-12-19 13:54:48 +0200

    simplify list component

--- src/screens/Orderbook/components/List/index.tsx
+++ src/screens/Orderbook/components/List/index.tsx
@@ -1,9 +1,7 @@
-import _ from 'lodash';
 import React, {FC} from 'react';
 import {Box} from '~/components/Box';
 import {Level} from '~/providers/Orderbook/types';
 import {LevelType} from '~/providers/Orderbook/types/enums';
-import {useVisibleItemsCount} from './hooks/useVisibleItemsCount';
 import {OrderbookListItem} from './Item';
 
 type Props = {
@@ -13,18 +11,18 @@ type Props = {
 };
 
 export const List: FC<Props> = ({items, type, highestTotal}) => {
-  const {visibleItemsInListCount} = useVisibleItemsCount();
-
   return (
     <Box marginX={-6}>
-      {_.take(items, visibleItemsInListCount).map(item => (
-        <OrderbookListItem
-          key={item.price}
-          highestTotal={highestTotal}
-          levelType={type}
-          priceLevel={item}
-        />
-      ))}
+      {items.map(item => {
+        return (
+          <OrderbookListItem
+            key={item.price}
+            highestTotal={highestTotal}
+            levelType={type}
+            priceLevel={item}
+          />
+        );
+      })}
     </Box>
   );
 };

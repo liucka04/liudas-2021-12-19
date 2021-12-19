0d1537dd28cd7e5df2ae64d58a732d09f0b05806
Liudas Barcauskas <liudasbarcauskas@gmail.com>
2021-12-19 13:54:31 +0200

    add environment config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
+const environment = process.env.ENVIRONMENT || 'development';
+const envPath = path.join(__dirname, `.env.${environment}`);
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    ['inline-dotenv', {path: envPath, systemVar: 'overwrite'}],
     [
       'module-resolver',
       {
---
+++ src/types/environment.d.ts
@@ -0,0 +1,7 @@
+declare var process: {
+  env: {
+    NODE_ENV: string;
+    ENVIRONMENT: string;
+    SOCKET_URL: string;
+  };
+};

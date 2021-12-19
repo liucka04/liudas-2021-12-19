2d6885846ddd272eb13be8610cac126b342285df
Liudas Barcauskas <liudasbarcauskas@gmail.com>
2021-12-20 01:18:04 +0200

    throttle product change clicks

--- src/providers/Orderbook/hooks/useToggleProduct.ts
+++ src/providers/Orderbook/hooks/useToggleProduct.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {useCallback} from 'react';
 import {OrderbookAction, ProductId} from '../types/enums';
 import {subscribeProductEvent, unsubscribeProductEvent} from '../utils/events';
@@ -8,35 +9,40 @@ export const useToggleProduct = () => {
     useOrderbookContext();
 
   const toggleProduct = useCallback(() => {
-    if (!isConnected || !socket) {
-      return;
-    }
+    _.throttle(() => {
+      if (!isConnected || !socket) {
+        return;
+      }
 
-    dispatch({
-      type: OrderbookAction.SET_LOADING,
-      isLoading: true,
-    });
+      dispatch({
+        type: OrderbookAction.SET_LOADING,
+        isLoading: true,
+      });
 
-    dispatch({
-      type: OrderbookAction.SET_PRICE_LEVELS,
-      priceLevels: {
-        asks: [],
-        bids: [],
-      },
-    });
+      dispatch({
+        type: OrderbookAction.SET_PRICE_LEVELS,
+        priceLevels: {
+          asks: [],
+          bids: [],
+        },
+      });
 
-    const nextProductId =
-      productId === ProductId.XBTUSD ? ProductId.ETHUSD : ProductId.XBTUSD;
+      const nextProductId =
+        productId === ProductId.XBTUSD ? ProductId.ETHUSD : ProductId.XBTUSD;
 
-    socket.send(unsubscribeProductEvent({productIds: [productId]}));
-    socket.send(subscribeProductEvent({productIds: [nextProductId]}));
+      socket.send(unsubscribeProductEvent({productIds: [productId]}));
+      socket.send(subscribeProductEvent({productIds: [nextProductId]}));
 
-    dispatch({type: OrderbookAction.SET_PRODUCT_ID, productId: nextProductId});
+      dispatch({
+        type: OrderbookAction.SET_PRODUCT_ID,
+        productId: nextProductId,
+      });
 
-    dispatch({
-      type: OrderbookAction.SET_LOADING,
-      isLoading: false,
-    });
+      dispatch({
+        type: OrderbookAction.SET_LOADING,
+        isLoading: false,
+      });
+    }, 200)();
   }, [dispatch, isConnected, productId, socket]);
 
   return {toggleProduct, isLoading};
